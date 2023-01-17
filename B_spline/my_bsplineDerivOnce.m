e8615fde4aee7964b0e6f83090932911f9bfe69d
TangKai-HIT <1292737216@qq.com>
2023-01-17 14:03:01 +0800

    first commit

---
+++ B_spline/my_bsplineDerivOnce.m
@@ -0,0 +1,3 @@
+function [new_control_pts, new_u_vec] = my_bsplineDerivOnce(control_pts, u_vec, u)
+%MY_BSPLINEDERIVONCE get derivative of the p-degree spline once, return equivalent control points and knot vector
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Curves
+Some basic algorithms of commonly-used curves for construction, interpolation and fitting. (In Matlab)  
+
+**Curves types**:
+- `Cubic spline`    
+- `B-spline`  
+- `NURBs`  
+- `Bezier`
