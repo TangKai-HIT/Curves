bdf10fb2295a48f0ff27e607f05f926b4b10a614
TangKai-HIT <1292737216@qq.com>
2023-02-13 14:45:07 +0800

    better example

--- Bezier/demos/demo1_bezier_surface.m
+++ Bezier/demos/demo1_bezier_surface.m
@@ -1,15 +1,15 @@
 %demo1: generate a Bezier controlled surface
 addpath(genpath('../'));
-%% init params 
-p = 3; q=3;
 
-control_pts = zeros(p+1,q+1,3);
-rng(2);
-for i = 1:p+1
-    for j=1:q+1
-        control_pts(i,j,:) = [i, j, randn(1)];
-    end
-end
+%% init params 
+surface_ctrl_pts = {[0, 0, 15], [0, 50, 65], [0, 102, 2.3], [0, 150, 15];
+                                   [76, 0, 65], [50, 76, -10], [50,102, 65], [89,150,53];
+                                   [102, 0, -5.5], [102,102, 40], [102, 76, -10], [127,150,15];
+                                   [150, 0,15], [150,12.7, -10], [150, 76, 48], [150,150,15]};
+% surface_ctrl_pts = surface_ctrl_pts';
+surface_ctrl_pts = ptCell2tensor(surface_ctrl_pts); %load to tensor
+
+m1 = 3; m2=3; %bicubic bezier
 
 %% show surface
-plotBezierSurface(gca, p, q, control_pts, 0:0.02:1, 0:0.02:1);
+plotBezierSurface(gca, m1, m2, surface_ctrl_pts, 0:0.02:1, 0:0.02:1);
