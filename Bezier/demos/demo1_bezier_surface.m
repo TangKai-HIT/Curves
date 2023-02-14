22031d4e4438f0361ec3d3a1b2c4de19d5b3a296
TangKai-HIT <1292737216@qq.com>
2023-02-14 17:07:29 +0800

    new contents

--- Bezier/demos/demo1_bezier_surface.m
+++ Bezier/demos/demo1_bezier_surface.m
@@ -13,3 +13,28 @@ m1 = 3; m2=3; %bicubic bezier
 
 %% show surface
 plotBezierSurface(gca, m1, m2, surface_ctrl_pts, 0:0.02:1, 0:0.02:1);
+
+%% show an isoparametric curve & sampled points coordinate frame
+fix_v = 0.6;
+equal_CtrlPts = bezierSurface_isoParamCurve(surface_ctrl_pts, fix_v, 'v');
+
+N = 100;
+u = linspace(0,1,N);
+isoCurvePts = bezierEval(m1, u, equal_CtrlPts);
+%plot curve
+hold on;
+plot3(gca, isoCurvePts(:,1), isoCurvePts(:,2), isoCurvePts(:,3), "-m", "LineWidth", 2.5);
+
+%plot sampled points coordinate frame
+u_sample = u(1:10:end);
+u_samplePts = bezierEval(m2, u_sample, equal_CtrlPts);
+
+e_u = zeros(length(u_sample), 3);
+e_v = zeros(length(u_sample), 3);
+n = zeros(length(u_sample), 3);
+
+for i = 1:length(u_sample)
+    [e_u(i, :), e_v(i, :), n(i, :)] = bezierSurface_getTanNorm(surface_ctrl_pts, u_sample(i), fix_v);
+end
+
+plotTanNormVec(gca, e_u, e_v, n, u_samplePts);
