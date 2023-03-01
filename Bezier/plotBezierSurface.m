1359a9e151d86e9e331f9df5da9bbb513bd73887
TangKai-HIT <1292737216@qq.com>
2023-03-01 15:21:26 +0800

    add hold

--- Bezier/plotBezierSurface.m
+++ Bezier/plotBezierSurface.m
@@ -3,6 +3,7 @@ function plotBezierSurface(ax, p, q, control_pts, u_range, v_range)
 %   input:
 %       u_range, v_range: sampled points on param range
 
+hold(ax,"on");
 % u_samp = linspace(u_range(1), u_range(2), numSamples);
 % v_samp = linspace(v_range(1), v_range(2), numSamples);
 pts = bezierSurfaceEval(p, q, control_pts, u_range, v_range);
